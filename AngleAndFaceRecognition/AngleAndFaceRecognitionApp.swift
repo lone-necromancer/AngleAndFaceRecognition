e1a9e8e97bca1ff0a6fbf12659a7724d32c81c9b
Aviv Frenkel <frenkel.a.7@gmail.com>
2023-06-11 03:53:48 +0300

    Initial Commit

---
+++ AngleAndFaceRecognition/AngleAndFaceRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AngleAndFaceRecognitionApp.swift
+//  AngleAndFaceRecognition
+//
+//  Created by Aviv Frenkel on 11/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct AngleAndFaceRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AngleAndFaceRecognition/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AngleAndFaceRecognition
+//
+//  Created by Aviv Frenkel on 11/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
